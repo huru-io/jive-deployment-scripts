f36646580347488380480e6c16930f05ed20a938
edovale <edovale@gmail.com>
2016-05-30 12:19:45 -0400

    Copying plugin artifacts to plugins directory.

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,13 @@
 #!/bin/bash
 
+ARTIFACTS_FOLDER=artifacts
+JIVE_HOME=/usr/local/jive
+
 sudo su - jive -c "jive stop"
 sudo su - jive -c "rm -rf ~/applications/sbs/home/plugins/*"
+
+sudo cp $ARTIFACTS_FOLDER/*.* $JIVE_HOME/applications/sbs/home/plugins/
+sudo chown jive:jive $JIVE_HOME/applications/sbs/home/plugins/*.*
 
 sudo su - postgres -c "psql -d jive -c 'delete from jiveplugin'"
 sudo su - jive -c "jive start"
