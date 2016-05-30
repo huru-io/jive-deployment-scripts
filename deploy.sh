71f6b7a754677dfa55e59f3f2176dde3f316bc07
edovale <edovale@gmail.com>
2016-05-30 16:14:56 -0400

    Adding theme deployment.

--- deploy.sh
+++ deploy.sh
@@ -2,12 +2,16 @@
 
 ARTIFACTS_FOLDER=artifacts
 JIVE_HOME=/usr/local/jive
+SBS_HOME=$JIVE_HOME/applications/sbs/home
 
 sudo su - jive -c "jive stop"
-sudo su - jive -c "rm -rf ~/applications/sbs/home/plugins/*"
+sudo su - jive -c "rm -rf $SBS_HOME/plugins/*"
+sudo su - jive -c "rm -rf $SBS_HOME/themes/*"
 
-sudo cp $ARTIFACTS_FOLDER/*.* $JIVE_HOME/applications/sbs/home/plugins/
-sudo chown jive:jive $JIVE_HOME/applications/sbs/home/plugins/*.*
+sudo cp $ARTIFACTS_FOLDER/*.jar $SBS_HOME/plugins/
+sudo unzip $ARTIFACTS_FOLDER/*.zip -d $SBS_HOME/themes/
+sudo chown jive:jive $SBS_HOME/plugins/*.*
+sudo chown -r jive:jive $SBS_HOME/themes/*
 
 sudo su - postgres -c "psql -d jive -c 'delete from jiveplugin'"
 sudo su - jive -c "jive start"
